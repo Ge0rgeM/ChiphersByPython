005b866d1e443f1d345200490df367125169b5d4
Ge0rgeM <99915251+Ge0rgeM@users.noreply.github.com>
2023-10-16 15:53:49 +0400

    Update MaterialsforProject.py

--- MaterialsforProject.py
+++ MaterialsforProject.py
@@ -20,7 +20,7 @@ def caesar_letter(letter,task="encryption"): # We pass 2 arguments: letter, that
                 return (caesar_AZ[normal_AZ.find(letter.upper())]).lower()
         else: # If letter is actually a symbol. We DO NOT change it
             return letter
-    else : #For Decryption
+    elif task.lower() == "decryption": #For Decryption
         if (letter>='A' and letter<='Z') or (letter >= 'a' and letter <='z'): #We only Encrypt letters and not symbols nor spaces...
             if letter.isupper():
                 #Since we store letters in uppercase (in our variables) 
@@ -38,27 +38,61 @@ def caesar_letter(letter,task="encryption"): # We pass 2 arguments: letter, that
             return letter
         
 
-def vignere_letter(letter, key, curr_index, task = "encryption"):
+#letter fo exryption or decryption
+#key
+#curr_index to match index of letter in the word to the index of letter in key, for easier encryption and decryption
+#task by default is encryption
+def vigenere_letter(letter, key, curr_index, task = "encryption"): 
     if task.lower() == "encryption":
         if (letter>='A' and letter<='Z') or (letter >= 'a' and letter <='z'):
             if letter.isupper():
-                return chr(ord('A')+(ord(letter)+ord(key[curr_index].upper()))%26)
+                return chr(ord('A')+(ord(letter)+ord(key[curr_index].upper()))%26) #we need to move from 'A' letter (word[i]+key[i])%26 times
             elif letter.islower():
-                return chr(ord('A')+(ord(letter.upper())+ord(key[curr_index].upper()))%26).lower()
+                return chr(ord('A')+(ord(letter.upper())+ord(key[curr_index].upper()))%26).lower() #we need to move from 'A' letter (word[i]+key[i])%26 times, but return lowercase
                 
         else: # If letter is actually a symbol. We DO NOT change it
             return letter
     elif task.lower() == "decryption" : #For Decryption
         if (letter>='A' and letter<='Z') or (letter >= 'a' and letter <='z'):
             if letter.isupper():
-                return chr(ord('A') + ((ord(letter) - ord((key[curr_index]).upper()))%26))
+                return chr(ord('A') + ((ord(letter) - ord((key[curr_index]).upper()))%26)) #we need to move from 'A' letter (word[i]-key[i])%26 times
             elif letter.islower():
-                return chr(ord('A') + ((ord(letter.upper()) - ord((key[curr_index]).upper())))%26).lower()
+                return chr(ord('A') + ((ord(letter.upper()) - ord((key[curr_index]).upper())))%26).lower() #we need to move from 'A' letter (word[i]-key[i])%26 times, but return lowercase
         else: # If letter is actually a symbol. We DO NOT change it
             return letter
 
-def check_symbol(symbol):
+def check_symbol(symbol): #checks symbol is letter or not
     if (symbol>='A' and symbol<='Z') or (symbol >= 'a' and symbol <='z'):
         return "Letter"
     else:
         return "Extra Symbol"
+    
+def greeting():
+    print("")
+    print("...........................................................................................................")
+    print("|>---HH----------HH------EEEEEEEEEEEEE------LL-------------------LL------------------------OOOOOOO--------|")
+    print("|>---HH----------HH------EE-----------------LL-------------------LL----------------------OO-------OO------|")
+    print("|>---HH----------HH------EE-----------------LL-------------------LL--------------------OO-----------OO----|")
+    print("|>---HH----------HH------EE-----------------LL-------------------LL-------------------OO-------------OO---|")
+    print("|>---HHHHHHHHHHHHHH------EEEEEEEEEEEEE------LL-------------------LL-------------------OO-------------OO---|")
+    print("|>---HH----------HH------EE-----------------LL-------------------LL-------------------OO-------------OO---|")
+    print("|>---HH----------HH------EE-----------------LL-------------------LL--------------------OO-----------OO----|")
+    print("|>---HH----------HH------EE-----------------LL-------------------LL---------------------OO---------OO-----|")
+    print("|>---HH----------HH------EEEEEEEEEEEEE------LLLLLLLLLLLLLLL------LLLLLLLLLLLLLLL----------OOOOOOOOO-------|")
+    print("...........................................................................................................")
+    print("")
+
+def goodbye():
+    print("")
+    print("...........................................................")
+    print("|>----BBBBBBBB---------YY--------YY------EEEEEEEEEEEEE----|")
+    print("|>----BB----BBBB--------YY------YY-------EE---------------|")
+    print("|>----BB------BBB--------YY----YY--------EE---------------|")
+    print("|>----BB-------BB---------YY--YY---------EE---------------|")
+    print("|>----BBBBBBBBBB-----------YYYY----------EEEEEEEEEEEEE----|")
+    print("|>----BB------_BB-----------YY-----------EE---------------|")
+    print("|>----BB------BBB-----------YY-----------EE---------------|")
+    print("|>----BB----BBBB------------YY-----------EE---------------|")
+    print("|>----BBBBBBBB--------------YY-----------EEEEEEEEEEEEE----|")
+    print("...........................................................")
+    print("")
